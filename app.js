c0df3cdd7a5702d106b7a3945cf531939783885f
wavinginspace <paulbaisley@gmail.com>
2020-01-15 12:44:46 -0500

    multiple dogs

--- app.js
+++ app.js
@@ -1,5 +1,3 @@
-'use strict';
-
 
 
 function callGetDogImage(){
@@ -8,39 +6,30 @@ function callGetDogImage(){
     let num = $('#textInput').val();  
     getDogImage(num);
   });
-
 }
 
-
 function getDogImage(num) {
-   
   fetch(`https://dog.ceo/api/breeds/image/random/${num}`)
     .then(response => response.json())
     .then(responseJson => 
-      console.log(responseJson))
+      displayResults(responseJson))
     .catch(error => alert('Something went wrong. Try again later.'));
 }
+ 
 
-// function displayResults(responseJson) {
-//   console.log(responseJson);
-//   //replace the existing image with the new one
-//   $('.results-img').replaceWith(
-//     `<img src="${responseJson.message}" class="results-img">`
-//   )
-//   //display the results section
-//   $('.results').removeClass('hidden');
-// }
+function displayResults(responseJson) {
+  //replace the existing image with the new one
 
-// function watchForm() {
-//   $('form').submit(event => {
-//     event.preventDefault();
-//     getDogImage();
-//   });
-// }
+  // console.log(responseJson.message);
 
-// $(function() {
-//   console.log('App loaded! Waiting for submit!');
-//   watchForm();
-// });
+  // let dogResults = responseJson.message.forEach(dog => )
+
+  $('.results').html(
+    `<img src="${responseJson.message}" class="results-img">`
+  );
+  //display the results section
+  // $('.results').removeClass('hidden');
+
+}
 
 callGetDogImage();
